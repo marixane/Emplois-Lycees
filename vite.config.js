a33c06164f604352b8d0916f0f0f4d64a868ff97
marixane <prof.marwane@gmail.com>
2026-06-30 22:30:24 +0100

    Force Safari compatible production build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2017'
+    target: 'es2017',
+    cssTarget: 'safari13'
   },
   esbuild: {
     target: 'es2017'
